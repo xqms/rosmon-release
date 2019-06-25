c71c184374a9fd7fffa1135829b0a21d3658b29b
Max Schwarz <max.schwarz@ais.uni-bonn.de>
2019-06-25 16:21:21 +0200

    Rebase from 'upstream'

--- rosmon_core/src/monitor/node_monitor.h
+++ rosmon_core/src/monitor/node_monitor.h
@@ -148,11 +148,21 @@ public:
 	inline unsigned int restartCount() const
 	{ return m_restartCount; }
 
+    inline uint64_t memoryLimit()const
+    { return m_launchNode->memoryLimitByte();}
+
+    inline float cpuLimit()const
+    { return m_launchNode->cpuLimit();}
+
 	//@}
 
 	//! Node name
 	inline std::string name() const
 	{ return m_launchNode->name(); }
+
+	//! Node namespace
+	inline std::string namespaceString() const
+	{ return m_launchNode->namespaceString(); }
 
 	//! Node PID
 	inline int pid() const
