8ab78d8ba2560eabc624731eaa8aeb049a1ada9e
Max Schwarz <max.schwarz@ais.uni-bonn.de>
2019-10-29 18:22:00 +0100

    Rebase from 'release/melodic/rosmon_core'

--- src/ui.h
+++ src/ui.h
@@ -7,6 +7,7 @@
 #include "monitor/monitor.h"
 #include "fd_watcher.h"
 #include "terminal.h"
+#include "log_event.h"
 
 #include <ros/wall_timer.h>
 
@@ -23,25 +24,34 @@ public:
 	~UI();
 
 	void update();
-	void log(const std::string& channel, const std::string& str);
+	void log(const LogEvent& event);
 private:
 	struct ChannelInfo
 	{
 		ChannelInfo()
 		{}
 
-		ChannelInfo(uint32_t color)
-		 : labelColor(color)
+		ChannelInfo(Terminal* term)
+		 : parser{term}
 		{}
 
-		uint32_t labelColor;
+		ChannelInfo(Terminal* term, uint32_t color)
+		 : parser{term}
+		 , labelColor(color)
+		{}
+
 		Terminal::Parser parser;
+		uint32_t labelColor;
 	};
 
 	void drawStatusLine();
 	void checkWindowSize();
 	void setupColors();
-	void handleInput();
+
+	void readInput();
+	void checkTerminal();
+
+	void handleKey(int key);
 
 	inline bool anyMuted()
 	{ return !m_mutedSet.empty(); }
@@ -68,6 +78,7 @@ private:
 
 	int m_columns;
 	ros::WallTimer m_sizeTimer;
+	ros::WallTimer m_terminalCheckTimer;
 
 	std::unordered_set<std::string> m_mutedSet;
 
@@ -76,6 +87,34 @@ private:
 	int m_selectedNode;
 
 	std::string m_strSetColor;
+
+	bool m_searchActive = false;
+	std::string m_searchString;
+	unsigned int m_searchSelectedIndex;
+	std::vector<unsigned int> m_searchNodes;
+	unsigned int m_searchDisplayColumns = 0;
+
+	Terminal::Color m_color_bar;
+
+	Terminal::Style m_style_barLine;
+	Terminal::Style m_style_bar;
+	Terminal::Style m_style_barKey;
+	Terminal::Style m_style_barHighlight;
+
+	Terminal::Style m_style_nodeKey;
+	Terminal::Style m_style_nodeKeyMuted;
+
+	Terminal::Style m_style_nodeIdle;
+	Terminal::Style m_style_nodeRunning;
+	Terminal::Style m_style_nodeCrashed;
+	Terminal::Style m_style_nodeWaiting;
+
+	Terminal::Style m_style_nodeIdleFaded;
+	Terminal::Style m_style_nodeRunningFaded;
+	Terminal::Style m_style_nodeCrashedFaded;
+	Terminal::Style m_style_nodeWaitingFaded;
+
+	unsigned int m_nodeLabelWidth = 20;
 };
 
 }
