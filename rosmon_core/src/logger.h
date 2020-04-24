0910b392f5484d7e73894e578ee275d0434cfa1d
Max Schwarz <max.schwarz@ais.uni-bonn.de>
2020-04-24 11:37:27 +0200

    Revert "Trimmed the branch to only the rosmon sub directory"
    This reverts commit b8fbc65f7ec605092702aa54490e431ca053cbde.

---
+++ rosmon_core/src/logger.h
@@ -0,0 +1,35 @@
+// Logs all output to a log file for the run
+// Author: Max Schwarz <max.schwarz@uni-bonn.de>
+
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <string>
+
+namespace rosmon
+{
+
+/**
+ * @brief Write log messages into a log file
+ **/
+class Logger
+{
+public:
+	/**
+	 * @brief Constructor
+	 *
+	 * @param path Path to the output file
+	 **/
+	explicit Logger(const std::string& path, bool flush = false);
+	~Logger();
+
+	//! Log message
+	void log(const std::string& source, const std::string& msg);
+private:
+	FILE* m_file = nullptr;
+	bool m_flush = false;
+};
+
+}
+
+#endif
